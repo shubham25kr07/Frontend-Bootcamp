d7b80c5ca933e5063877c1630732b8df25f42f79
shubham25kr07 <shubham.k@razorpay.com>
2022-08-05 13:24:45 +0530

    added customer - items file structure

--- invoiceapp/src/components/Customer.js
+++ invoiceapp/src/components/Customer.js
@@ -1,3 +1,9 @@
-const Customer = () => {};
+import { useState } from "react";
+
+const Customer = () => {
+  // name ,  phone , email --> usestate
+  // call form compoent
+  // call table component
+};
 
 export default Customer;
