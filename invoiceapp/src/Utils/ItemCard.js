209e28f6ed4c105fde8d80a7f2346dca21afbea5
shubham25kr07 <shubham.k@razorpay.com>
2022-08-18 13:44:19 +0530

    feat: added functionality in invoice form

--- invoiceapp/src/Utils/ItemCard.js
+++ invoiceapp/src/Utils/ItemCard.js
@@ -1,10 +1,12 @@
-const ItemCard = ({ Name, Quantity, Price, classNameField, className }) => {
+const ItemCard = ({ Name, Quantity, Amount, className }) => {
   return (
     <div className={"item-card-field"}>
-      <div className="item-card-name">Model A</div>
+      <div className="item-card-name">{Name}</div>
       <div className="item-card-field-quantity-price">
-        <div className="item-card-quantity">x2</div>
-        <div className={className || "total-amount-default"}>$30000</div>
+        <div className="item-card-quantity">
+          {Quantity ? `x ${Quantity}` : ""}
+        </div>
+        <div className={className || "total-amount-default"}>{Amount}</div>
       </div>
     </div>
   );
