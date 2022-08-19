5ff4a06465979aeabf2d300d093b020787e7aa6c
shubham25kr07 <shubham.k@razorpay.com>
2022-08-19 15:44:13 +0530

    feat : added test file template

--- invoiceapp/src/App.js
+++ invoiceapp/src/App.js
@@ -1,47 +1,8 @@
 import InvoiceApp from "./InvoiceApp";
 import { createContext, useEffect, useState } from "react";
+import { CustomerApi, InvoiceApi, ItemApi } from "./Utils/Network";
 export const EntityDetailsContext = createContext();
-const CustomerApi = (setCustomerList) => {
-  const url = `http://localhost:8080/v1/customer/customerList?page=1`;
-  const data = {
-    sort_key: "name",
-    sort_value: "ASC",
-  };
-  fetch(url, {
-    method: "POST",
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((json) => setCustomerList(json.customer));
-};
-const ItemApi = (setItemList) => {
-  const url = `http://localhost:8080/v1/item/itemList?page=1`;
-  const data = {
-    sort_key: "item_name",
-    sort_value: "ASC",
-  };
-  fetch(url, {
-    method: "POST",
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      setItemList(json.item);
-    })
-    .catch((error) => console.table(error));
-};
-const InvoiceApi = (setInvoiceList) => {
-  const url = "http://localhost:8080/v1/invoice/list";
 
-  fetch(url, {
-    method: "GET",
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json);
-      setInvoiceList(json);
-    });
-};
 function App() {
   const [customerList, setCustomerList] = useState([]);
   const [itemList, setItemList] = useState([]);
