3aadf64b469c18f5d3b400246d8de739c591eb7a
pradeepp24 <pradeep.patidar@razorpay.com>
2022-08-25 10:43:50 +0530

    added invoiceform e2e test

--- invoiceapp/tests/example.spec.js
+++ invoiceapp/tests/example.spec.js
@@ -19,16 +19,17 @@ test.skip("item add page", async ({ page }) => {
 test("invoice add page", async ({ page }) => {
   await page.goto("http://localhost:3000/invoice/");
   await page.click("text = Add Invoice");
-  // await page.click("text = Change");
-  // await page.selectOption("#selectId", "2");
+  await page.click("text = Change");
+  await page.selectOption("#selectId", "2");
+  await page.locator("[data-test-id=save-button]").click();
   // await page.click('button:text("Add")');
   // await page.click("text = Add");
-  // await page.fill('[id="dueDate"]', "08/10/2022");
-  // await page.click("text = Add an Item");
-  // await page.selectOption("#selectId", ["0", "1"]);
-  // await page.click('button:text("Add")');
-  // await page.fill("input[name='0']", "3");
-  // await page.fill("textarea[name='Item_Description']", "This is invoice");
-  // await page.click('button:text("Save Invoice")');
+  await page.fill('[id="dueDate"]', "08/10/2022");
+  await page.click("text = Add an Item");
+  await page.selectOption("#selectId", ["0", "1"]);
+  await page.locator("[data-test-id=save-button]").click();
+  await page.fill("input[name='0']", "3");
+  await page.fill("textarea[name='Notes']", "This is invoice");
+  await page.click('button:text("Save Invoice")');
   // await page.waitForTimeout(5000);
 });
